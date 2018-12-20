41abe906ff8e5c4ef327f782ce525f953a82e8b6
Youngsup Shin <youngsup.shin@mail.utoronto.ca>
2018-12-19 19:33:31 -0500

    Added arpspoof_detector.py

---
+++ arpspoof_detector.py
@@ -0,0 +1,13 @@
+import scapy.all as scapy
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+        print(packet.show())
+
+
+sniff("eth0")
